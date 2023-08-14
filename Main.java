d6bf888ab7620e390bfda6bbc43c428b53d83632
Ka1ine <yasminkaline73@gmail.com>
2023-08-13 21:05:16 -0300

    Criando os primeiros objetos

---
+++ Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] arg){
+
+        // Instanciando o primeiro livro da biblioteca.
+        Book myBook = new Book(
+                                "A garota do lago", 
+                                "Summit Lake, uma pequena cidade entre montanhas", 
+                                "Padrão", 
+                                "Português", 
+                                "Charlie Donlea",
+                                "Faro Editorial",
+                                123,
+                                459,
+                                16
+                            );
+
+        String title = myBook.getTitle();
+        String author = myBook.getAuthor();
+
+        System.out.println("O livro " + title + " escrito por " + author + " foi o primeiro livro que chegou na bliblioteca.");
+        
+        
+        //Instanciando a primeira cliente da biblioteca
+        Client myClient = new Client("Maria", 1234);
+
+        String name = myClient.getName();
+        System.out.println(name + " primeira cliente da biblioteca.");
+
+    }
+}
