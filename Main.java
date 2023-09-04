dafdcf4def75af76484fb559843b98b51c905d84
Ka1ine <yasminkaline73@gmail.com>
2023-09-03 22:00:10 -0300

    Criando instancias

---
+++ Main.java
@@ -0,0 +1,62 @@
+import java.time.LocalDate;
+
+import member.Employee;
+import member.People;
+import multimedia.Book;
+
+public class Main {
+    public static void main(String[] arg){
+        People pessoa = new People(
+        "João", 
+        12345, 
+        "Rua Exemplo, 123", 
+        "joao@example.com", 
+        LocalDate.of(
+            2023, 
+            9, 
+            3
+        )
+        );
+
+        Employee funcionario = new Employee(
+            "Maria", 
+            54321, 
+            "Rua Exemplo, 456", 
+            "maria@example.com",
+            LocalDate.of(
+                2023, 
+                9, 
+                3
+            ), 
+            "Gerente de Projetos"
+        );
+
+        Book livro = new Book(
+            "O Grande Gatsby", 
+            "F. Scott Fitzgerald", 
+            "Scribner", 
+            1925, 
+            "Ficção",
+            "Um romance sobre o Sonho Americano", 
+            "Inglês", 
+            "Capa dura", 
+            "978-3-16-148410-0", 
+            "Primeira Edição",
+            100, 
+            50, 
+            "Estante da Biblioteca 3A", 
+            "Bom estado"
+        );
+
+        Borrow emprestimo = new Borrow(
+            pessoa, 
+            livro, 
+            funcionario, 
+            "2023-09-03", 
+            "2023-09-10"
+        );
+        
+        System.out.println("Data de emprestimo: " + emprestimo.getDataDevolucao());
+
+    }
+}
